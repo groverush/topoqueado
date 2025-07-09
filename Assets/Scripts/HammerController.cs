cd0ee5d5bd27b3587fbd8e075545890df5c1fb5e
sebascg17 <sebas-clavi@hotmail.com>
2025-07-09 15:21:47 -0500

    I correct the top of the box with their holes

--- Assets/Scripts/HammerController.cs
+++ Assets/Scripts/HammerController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Hammer : MonoBehaviour
+public class HammerController : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
