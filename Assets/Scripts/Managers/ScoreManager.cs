780dc1189dde794da8f5401832ac3ff0358d730a
William Ardila Sánchez <103336696+WillyArdilaS@users.noreply.github.com>
2025-07-16 13:06:48 -0500

    Added: Fonts, audio assets and score manager with UI integration (#20)
    * Fix: Input system is working correctly in the prototype
    
    * Update InputInitializer script
    
    * Separate managers and collision detection
    
    * Added: Fonts, audio assets and score manager with UI integration

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -1,6 +1,49 @@
+using System;
 using UnityEngine;
 
 public class ScoreManager : MonoBehaviour
 {
+    // === Managers ===
+    [SerializeField] private CollisionManager collisionManager;
+    [SerializeField] private UIManager uiManager;
 
+    // === Score ===
+    private int hammerScore = 0;
+    private int moleScore = 0;
+
+    // === Events ===
+    public event Action<int, Vector3> OnHammerScoreChanged;
+    public event Action<int, Vector3> OnMoleScoreChanged;
+
+    void Awake()
+    {
+        if (collisionManager != null)
+        {
+            collisionManager.OnHitSuccess += AddHammerScore;
+            collisionManager.OnHitMiss += AddMoleScore;
+        }
+    }
+
+    private void AddHammerScore(HammerController hammer)
+    {
+        Vector3 hammerHolePosition = hammer.holeNavigationScript.CurrentHole.transform.position;
+        hammerScore++;
+        OnHammerScoreChanged?.Invoke(hammerScore, hammerHolePosition);
+    }
+
+    private void AddMoleScore(MoleController mole)
+    {
+        Vector3 moleHolePosition = mole.holeNavigationScript.CurrentHole.transform.position;
+        moleScore++;
+        OnMoleScoreChanged?.Invoke(moleScore, moleHolePosition);
+    }
+
+    private void ResetScores()
+    {
+        hammerScore = 0;
+        moleScore = 0;
+
+        OnHammerScoreChanged?.Invoke(hammerScore, Vector3.zero);
+        OnMoleScoreChanged?.Invoke(moleScore, Vector3.zero);
+    }
 }
